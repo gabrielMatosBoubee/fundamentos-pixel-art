b5635ec5bd79719d38d61672aa6663714b3dcb9b
petromamboubee <gm.boubee@gmail.com>
2022-08-02 19:12:52 -0300

    terminei o 6, o 7 deixei no centro os elementos.

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function quadroEmBranco (parametro){
-    let localização = document.GetElementsByClassName('pixel')
-    let localizaçãoPai = document.getElementById('pixel-board')
-    for (let index = 0; index<parametro.length; index += 1) {
-        for(let i = 0; i<parametro.length; index += 1){
-       let pixel = document.createElement('div')}
-    }
+window.onload = function(){
+  function resultado (){
+  let preto = document.getElementsByClassName('color')[0]
+  preto.classList.add('selected')
 }
+resultado()
+}
+let x = document.getElementById('color-palette')
+function mudarCor (parametro) {
+let selected = document.querySelector('.selected')
+selected.classList.remove('selected')
+parametro.target.classList.add('selected')
+} 
+x.addEventListener("click", mudarCor)
+
+//function quadroEmBranco (parametro){
+//    let localização = document.GetElementsByClassName('pixel')
+//    let localizaçãoPai = document.getElementById('pixel-board')
+//    for (let index = 0; index<parametro.length; index += 1) {
+//        for(let i = 0; i<parametro.length; index += 1){
+//       let pixel = document.createElement('div')}
+//    }
+//}
