771ad331df77702e58a20b16d37a852be2d93ceb
petromamboubee <gm.boubee@gmail.com>
2022-08-02 18:14:40 -0300

    conclui até a parte 4

---
+++ script.js
@@ -0,0 +1,8 @@
+function quadroEmBranco (parametro){
+    let localização = document.GetElementsByClassName('pixel')
+    let localizaçãoPai = document.getElementById('pixel-board')
+    for (let index = 0; index<parametro.length; index += 1) {
+        for(let i = 0; i<parametro.length; index += 1){
+       let pixel = document.createElement('div')}
+    }
+}
